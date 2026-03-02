1a5d7d5ec042cfe6379371a712918d1c409ba024
Andrzej Rzeszutek <andrzej.rzeszutek@gmail.com>
2026-03-02 23:41:43 +0100

    Add code to remove 'active' class

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
 'use strict';
-// document.getElementById('test-button').addEventListener('click', function() {
 
-//     const links = document.querySelectorAll('.titles a');
-//     console.log('links:', links);
-// });
 const titleClickHandler = function(event){
     console.log(event);
 
     /* remove class 'active' from all article links  */
-
+    const activeLinks = document.querySelectorAll('.titles a.active');
+    
+    for (let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
+    
     /* add class 'active' to the clicked link */
 
     /* remove class 'active' from all articles */
-
+    const activeArticles = document.querySelectorAll('.posts article.active');
+    console.log(activeArticles);
+    for (let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
+    console.log(activeArticles);
     /* get 'href' attribute from the clicked link */
 
     /* find the correct article using the selector (value of 'href' attribute) */
