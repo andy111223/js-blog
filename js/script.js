aad034d41b773c022ca39d19c9ea70238aa5ba4d
Andrzej Rzeszutek <andrzej.rzeszutek@gmail.com>
2026-03-02 23:59:19 +0100

    Add 'active' class to clickedElement

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 'use strict';
 
 const titleClickHandler = function(event){
-    console.log(event);
+    const clickedElement = this;
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
-    
     for (let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
     
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement);
+    clickedElement.classList.add('active');
 
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
-    console.log(activeArticles);
     for (let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }
-    console.log(activeArticles);
+
     /* get 'href' attribute from the clicked link */
 
     /* find the correct article using the selector (value of 'href' attribute) */
@@ -29,5 +29,6 @@ const titleClickHandler = function(event){
 
 const links = document.querySelectorAll('.titles a');
 for (let link of links) {
+    console.log('link:', link);
     link.addEventListener('click', titleClickHandler);
 }    
