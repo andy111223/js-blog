5046cbcac1466c0e5b3c56369d274234832e5655
Andrzej Rzeszutek <andrzej.rzeszutek@gmail.com>
2026-03-04 03:11:28 +0100

    Add eslint, editorconfig, stylelint. Update package.json. Correct errors

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-{
-'use strict';
+(()=>{
+  'use strict';
 
-
-const titleClickHandler = function(event){
+  const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
 
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
     for (let activeLink of activeLinks){
-        activeLink.classList.remove('active');
+      activeLink.classList.remove('active');
     }
     
     /* add class 'active' to the clicked link */
@@ -18,7 +17,7 @@ const titleClickHandler = function(event){
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
     for (let activeArticle of activeArticles){
-        activeArticle.classList.remove('active');
+      activeArticle.classList.remove('active');
     }
 
     /* get 'href' attribute from the clicked link */
@@ -29,15 +28,15 @@ const titleClickHandler = function(event){
 
     /* add class 'active' to the correct article */
     targetArticle.classList.add('active');
-}
+  };
 
-/* constants for options that may change */
-const optArticleSelector = '.posts article',
-      optTitleSelector = '.post-title',
-      optTitleListSelector = '.titles';
+  /* constants for options that may change */
+  const optArticleSelector = '.posts article',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
 
-/* Function to generate title links */
-function generateTitleLinks() {
+  /* Function to generate title links */
+  function generateTitleLinks() {
 
     /* remove contents of titleList */
     const titleList = document.querySelector(optTitleListSelector);
@@ -48,20 +47,20 @@ function generateTitleLinks() {
     const articles = document.querySelectorAll(optArticleSelector);
     for (let article of articles) {
 
-        /* read ID and save to a constant*/
-        const articleId = article.getAttribute('id');
+      /* read ID and save to a constant*/
+      const articleId = article.getAttribute('id');
 
-        /* find title and save to a constant */
-        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+      /* find title and save to a constant */
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
-        /* create HTML link and save to a constant */
-        const linkHTML = `<li><a href="#${articleId}"><span>${articleTitle}</span></a></li>`;
+      /* create HTML link and save to a constant */
+      const linkHTML = `<li><a href="#${articleId}"><span>${articleTitle}</span></a></li>`;
 
-        // /* insert the HTML link to titleList */
-        // titleList.insertAdjacentHTML('beforeend', linkHTML);
+      // /* insert the HTML link to titleList */
+      // titleList.insertAdjacentHTML('beforeend', linkHTML);
 
-        /* insert link into html variable */
-        html = html + linkHTML;
+      /* insert link into html variable */
+      html = html + linkHTML;
     }
     /* insert the generated HTML into ul element */
     titleList.innerHTML = html;
@@ -69,10 +68,9 @@ function generateTitleLinks() {
     /* create a list of links by slecting a's from ul, and add event listeners */
     const links = document.querySelectorAll('.titles a');
     for (let link of links) {
-        link.addEventListener('click', titleClickHandler);
+      link.addEventListener('click', titleClickHandler);
     } 
-}
+  }
 
-generateTitleLinks();
-
-}
+  generateTitleLinks();
+})();
